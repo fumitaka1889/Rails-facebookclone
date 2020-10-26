4a5e201ea8852c9cfd19ca324352f337c332416b
fimitaka shimizu <gilgala.aiglos@gmail.com>
2020-10-26 15:41:31 +0900

    index画面にリンクを貼る及び削除機能追加

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,21 +1,35 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: [:edit, :show, :destroy]
   def index
+    @notes = Note.all
   end
+
   def new
     @note = Note.new
   end
 
-  def edit
-    @note = Note.find(params[:id])
-  end
-
   def create
     Note.create(note_params)
-    redirect_to notes_path
+    redirect_to notes_path, notice:"画像を投稿しました！"
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to notes_path, notice:"投稿を削除しました！"
   end
 
   private
   def note_params
     params.require(:note).permit(:title, :image, :image_cache, :content)
+  end
+
+  def set_note
+    @note = Note.find(params[:id])
   end
 end
