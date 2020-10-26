de0b9b851382eced52fc49376e14cfe1c6f7f690
fimitaka shimizu <gilgala.aiglos@gmail.com>
2020-10-26 13:28:25 +0900

    画像投稿・編集機能・index表示画面作成

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,21 @@
 class NotesController < ApplicationController
+  def index
+  end
   def new
+    @note = Note.new
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def create
+    Note.create(note_params)
+    redirect_to notes_path
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :image, :image_cache, :content)
   end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,4 @@
 class Note < ApplicationRecord
   mount_uploader :image, ImageUploader
-  belongs_to :user
+  # belongs_to :user
 end
